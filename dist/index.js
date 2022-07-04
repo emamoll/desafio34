04d83680ff0e6d7bd6efbbd429c0116c521e1c27
Emanuel <emanuelgmoll@gmail.com>
2022-07-03 23:30:13 -0300

    cambios en el index

--- dist/index.js
+++ dist/index.js
@@ -2,24 +2,49 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _db = require("./services/db");
-
 var _server = _interopRequireDefault(require("./services/server"));
 
-var _console = require("./console");
+var _cluster = _interopRequireDefault(require("cluster"));
 
+var _os = _interopRequireDefault(require("os"));
+
+var _minimist = _interopRequireDefault(require("minimist"));
+
+// import { connectDb } from "./services/db";
+// import { logger } from "./console";
+var argumentos = (0, _minimist["default"])(process.argv.slice(2));
 var PORT = process.env.PORT || 8080;
+var clusterMode = argumentos.cluster; //Obtengo el numero de nucleos disponibles en mi PC
 
-var init = function init() {
-  (0, _db.connectDb)();
+var numCPUs = _os["default"].cpus().length;
+/* --------------------------------------------------------------------------- */
 
-  var server = _server["default"].listen(PORT, function () {
-    return _console.logger.info("Escuchando en puerto ".concat(PORT, " - PID Worker ").concat(process.pid));
+/* MASTER */
+
+/**
+ * isMaster vs isPrimary
+ * https://stackoverflow.com/questions/68978929/why-is-nodejs-cluster-module-not-working
+ */
+
+
+if (clusterMode && _cluster["default"].isMaster) {
+  console.log('Ejecutando modo cluster');
+  console.log("PID MASTER ".concat(process.pid));
+
+  for (var i = 0; i < numCPUs; i++) {
+    _cluster["default"].fork();
+  }
+
+  _cluster["default"].on('exit', function (worker) {
+    console.log("Worker ".concat(worker.process.pid, " died at ").concat(Date()));
+
+    _cluster["default"].fork();
   });
+} else {
+  /* --------------------------------------------------------------------------- */
 
-  server.on("error", function (err) {
-    return _console.logger.error("Error en el servidor: ".concat(err));
+  /* WORKERS */
+  _server["default"].listen(PORT, function () {
+    return console.log("Servidor express escuchando en el puerto ".concat(PORT, " - PID WORKER ").concat(process.pid));
   });
-};
-
-init();
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import { connectDb } from "./services/db";
+// import { connectDb } from "./services/db";
 import Server from "./services/server";
-import { logger } from "./console";
+import cluster from 'cluster';
+import os from 'os';
+import minimist from "minimist";
+// import { logger } from "./console";
 
+const argumentos = minimist(process.argv.slice(2));
 const PORT = process.env.PORT || 8080;
 
-const init = () => {
-  connectDb();
+const clusterMode = argumentos.cluster;
+//Obtengo el numero de nucleos disponibles en mi PC
+const numCPUs = os.cpus().length;
 
-  const server = Server.listen(PORT, () =>
-    logger.info(`Escuchando en puerto ${PORT} - PID Worker ${process.pid}`)
+/* --------------------------------------------------------------------------- */
+/* MASTER */
+/**
+ * isMaster vs isPrimary
+ * https://stackoverflow.com/questions/68978929/why-is-nodejs-cluster-module-not-working
+ */
+if (clusterMode && cluster.isMaster) {
+  console.log('Ejecutando modo cluster');
+  console.log(`PID MASTER ${process.pid}`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died at ${Date()}`);
+    cluster.fork();
+  });
+} else {
+  /* --------------------------------------------------------------------------- */
+  /* WORKERS */
+  Server.listen(PORT, () =>
+    console.log(
+      `Servidor express escuchando en el puerto ${PORT} - PID WORKER ${process.pid}`
+    )
   );
-
-  server.on("error", (err) => logger.error(`Error en el servidor: ${err}`));
-};
-
-init();
+}
