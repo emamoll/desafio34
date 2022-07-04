667cebdfc866a30263c3f12cbe70c13b7675bcef
Emanuel <emanuelgmoll@gmail.com>
2022-07-03 23:14:14 -0300

    Cambios en el index

--- dist/index.js
+++ dist/index.js
@@ -2,25 +2,19 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _config = _interopRequireDefault(require("./config"));
-
 var _db = require("./services/db");
 
 var _server = _interopRequireDefault(require("./services/server"));
 
-var _arguments = require("./arguments");
-
 var _console = require("./console");
 
-var _cluster = _interopRequireDefault(require("cluster"));
-
-var puerto = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 var init = function init() {
   (0, _db.connectDb)();
 
-  var server = _server["default"].listen(puerto, function () {
-    return _console.logger.info("Escuchando en puerto ".concat(puerto, " - PID Worker ").concat(process.pid));
+  var server = _server["default"].listen(PORT, function () {
+    return _console.logger.info("Escuchando en puerto ".concat(PORT, " - PID Worker ").concat(process.pid));
   });
 
   server.on("error", function (err) {
