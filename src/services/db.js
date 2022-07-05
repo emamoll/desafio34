47dcc5db856925ff7cbc3ecbc8df0a8566fbbea8
Emanuel <emanuelgmoll@gmail.com>
2022-07-04 21:36:13 -0300

    Revert cambio conexion

--- src/services/db.js
+++ src/services/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-// import Config from "../config";
+import Config from "../config";
 
 export const connectDb = () => {
-  return mongoose.connect('mongodb+srv://emamoll:plm_37616367@cluster0.pzs84.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+  return mongoose.connect(Config.MONGO_ATLAS_SRV, { useNewUrlParser: true });
 };
